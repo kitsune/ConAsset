ff65fefe56c5da1f631fda84accf54fa9bdcc2a3
Dylan Enloe <denloe@cs.pdx.edu>
2009-10-17 18:07:04 -0700

    Changed User.php to get users from the database

--- User.php
+++ User.php
@@ -44,29 +44,77 @@ class User {
 	}
 } */
 
+session_start();
+
+require_once 'Connection.php';
+
 class User {
 	private $admin;
 	private $user;
 	private $username;
+	private $barcode;
 	
 	function __construct() {
-	$self->admin = true;
-	$self->user = true;
-	$self->username = "kitten";
+		if(!isset($_SESSION['username'])) {
+			if(isset($_POST['login'])) {
+				$this->username = $_POST['username'];
+				$password = $_POST['password'];
+				$query = "
+				SELECT p_password, p_barcode
+				FROM people
+				WHERE p_username = '$this->username';";
+				$connection = new Connection();
+				$connection->query($query);
+				if($connection->result_size() == 1) {
+					$row = $connection->fetch_row();
+					if($row[0] == $password) {
+						$_SESSION['username'] = $username;
+						$_SESSION['barcode'] = $row[1];
+						$_SESSION['user'] = true;
+						$_SESSION['admin'] = true;
+					} else {
+						$this->loginError();
+						exit(0);
+					}
+				} else {
+					$this->loginError();
+					exit(0);
+				}
+			} else {
+				$this->printLoginScreen();
+				exit(0);
+			}
+		} else {
+			echo "logged in";
+		}
 	}
 	
-	public function is_Admin()
-	{
+	private function loginError() {
+		echo "
+<center>
+Could not log in, please check your user name and password and try again
+<br>
+<a href=\"index.php\">Back to login screen</a>
+</center>";
+	}
+	
+	private function printLoginScreen() {
+		echo "
+<form action=\"index.php?action=login\" method=\"post\" enctype=\"multipart/form-data\">
+Username: <input type=\"text\" name=\"username\"> <br>
+password: <input type=\"password\" name=\"password\"> <br>
+<input type=\"submit\" name=\"login\" value=\"Finished\">";
+	}
+	
+	public function is_Admin() {
 		return $this->admin;
 	}
 	
-	public function is_User()
-	{
+	public function is_User() {
 		return $this->user;
 	}
 	
-	public function get_Username()
-	{
+	public function get_Username() {
 		return $this->username;
 	}
 }
