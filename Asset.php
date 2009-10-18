87abbb22ed91ce994152d334586c29da1d52cad4
Dylan Enloe <denloe@cs.pdx.edu>
2009-10-17 22:29:41 -0700

    Rewrote most of Asset.php

--- Asset.php
+++ Asset.php
@@ -16,266 +16,211 @@
 
 class Asset {
 
-private $conn;
-private $aBarcode  = 0;
-private $aName;
-private $aDescription;
-private $aCondition;
-private $aCheckoutOutTo;
-private $aBox;
-private $aItemType;
+	private $connection;
+	private $barcode  = '';
+	private $name = '';
+	private $description = '';
+	private $condition = 1;
+	private $checkoutTo = '';
+	private $box = '';
+	private $itemType = '';
 
-/**
- * Retrieves the value from the field a_barcode
- * @return String with the value of the field
- */
-public function getABarcode(){
-  return $this->aBarcode;
-}
-/**
- * Retrieves the value from the field a_name
- * @return String with the value of the field
- */
-public function getAName(){
-  return $this->aName;
-}
-/**
- * Retrieves the value from the field a_description
- * @return String with the value of the field
- */
-public function getADescription(){
-  return $this->aDescription;
-}
-/**
- * Retrieves the value from the field a_condition
- * @return String with the value of the field
- */
-public function getACondition(){
-  return $this->aCondition;
-}
-/**
- * Retrieves the value from the field a_checkout_out_to
- * @return String with the value of the field
- */
-public function getACheckoutOutTo(){
-  return $this->aCheckoutOutTo;
-}
-/**
- * Retrieves the value from the field a_box
- * @return String with the value of the field
- */
-public function getABox(){
-  return $this->aBox;
-}
-/**
- * Retrieves the value from the field a_item_type
- * @return String with the value of the field
- */
-public function getAItemType(){
-  return $this->aItemType;
-}
-/**
- * Set the value from the field a_barcode
- * @param aBarcode String with the value for the field
- */
-public function setABarcode($aBarcode){
-  $this->aBarcode = $aBarcode;
-}
-/**
- * Set the value from the field a_name
- * @param aName String with the value for the field
- */
-public function setAName($aName){
-  $this->aName = $aName;
-}
-/**
- * Set the value from the field a_description
- * @param aDescription String with the value for the field
- */
-public function setADescription($aDescription){
-  $this->aDescription = $aDescription;
-}
-/**
- * Set the value from the field a_condition
- * @param aCondition String with the value for the field
- */
-public function setACondition($aCondition){
-  $this->aCondition = $aCondition;
-}
-/**
- * Set the value from the field a_checkout_out_to
- * @param aCheckoutOutTo String with the value for the field
- */
-public function setACheckoutOutTo($aCheckoutOutTo){
-  $this->aCheckoutOutTo = $aCheckoutOutTo;
-}
-/**
- * Set the value from the field a_box
- * @param aBox String with the value for the field
- */
-public function setABox($aBox){
-  $this->aBox = $aBox;
-}
-/**
- * Set the value from the field a_item_type
- * @param aItemType String with the value for the field
- */
-public function setAItemType($aItemType){
-  $this->aItemType = $aItemType;
-}
-/**
- * Default constructor
- * @param conn the database connection
- */
- function __construct($conn){
+	function __construct($connection){
+		$this->connection = $connection;
+	}
+	
+	public function getBarcode(){
+		return $this->barcode;
+	}
 
-      $this->conn = $conn;
-    
-}
-/**
- * Returns the string representation of this obbject
- * @return String repesentation ofAssets
- */
-public function toString(){
-  $s = '';
-  $s .= 'a_barcode: '.$this->aBarcode;
-  $s .= 'a_name: '.$this->aName;
-  $s .= 'a_description: '.$this->aDescription;
-  $s .= 'a_condition: '.$this->aCondition;
-  $s .= 'a_checkout_out_to: '.$this->aCheckoutOutTo;
-  $s .= 'a_box: '.$this->aBox;
-  $s .= 'a_item_type: '.$this->aItemType;
-  return $s;
-}
-/**
- * Initialize the business object with data read from the DB.
- * @param row array containing one read record.
- */
-private function init($row){
-  $this->aBarcode = $row['a_barcode'];
-  $this->aName = $row['a_name'];
-  $this->aDescription = $row['a_description'];
-  $this->aCondition = $row['a_condition'];
-  $this->aCheckoutOutTo = $row['a_checkout_out_to'];
-  $this->aBox = $row['a_box'];
-  $this->aItemType = $row['a_item_type'];
-}
-/**
- * Initialize the business object with data read from the DB.
- */
-private function initPOST(){
-  if (isset($_POST['aBarcode'])){
-    $this->aBarcode = $_POST['aBarcode'];
-  }
-  if (isset($_POST['aName'])){
-    $this->aName = $_POST['aName'];
-  }
-  if (isset($_POST['aDescription'])){
-    $this->aDescription = $_POST['aDescription'];
-  }
-  if (isset($_POST['aCondition'])){
-    $this->aCondition = $_POST['aCondition'];
-  }
-  if (isset($_POST['aCheckoutOutTo'])){
-    $this->aCheckoutOutTo = $_POST['aCheckoutOutTo'];
-  }
-  if (isset($_POST['aBox'])){
-    $this->aBox = $_POST['aBox'];
-  }
-  if (isset($_POST['aItemType'])){
-    $this->aItemType = $_POST['aItemType'];
-  }
-}
-/**
- * Initialize the business object with data read from the DB.
- */
-private function initGET(){
-  if (isset($_GET['aBarcode'])){
-    $this->aBarcode = $_GET['aBarcode'];
-  }
-  if (isset($_GET['aName'])){
-    $this->aName = $_GET['aName'];
-  }
-  if (isset($_GET['aDescription'])){
-    $this->aDescription = $_GET['aDescription'];
-  }
-  if (isset($_GET['aCondition'])){
-    $this->aCondition = $_GET['aCondition'];
-  }
-  if (isset($_GET['aCheckoutOutTo'])){
-    $this->aCheckoutOutTo = $_GET['aCheckoutOutTo'];
-  }
-  if (isset($_GET['aBox'])){
-    $this->aBox = $_GET['aBox'];
-  }
-  if (isset($_GET['aItemType'])){
-    $this->aItemType = $_GET['aItemType'];
-  }
-}
+	public function getName(){
+		return $this->name;
+	}
 
-public function loadAll(){
+	public function getDescription(){
+		return $this->description;
+	}
 
-      $rows = mysql_query("select * from assets", $this->conn);
-      $assetss = array();
-      while ($row = mysql_fetch_array($rows)) {
-        $d = new Assets($this->conn);
-        $d->init($row);
-        array_push($assetss,$d);
-      }
-      return $assetss;
-    
-}
-/**
- * Insert this object into the DB
- * @return new id (auto increment value) genereated
- */
-public function insert(){
+	public function getCondition(){
+		return $this->condition;
+	}
+	
+	public function getCheckoutTo(){
+		return $this->checkoutTo;
+	}
+	
+	public function getBox(){
+		return $this->box;
+	}
+	
+	public function getItemType(){
+		return $this->itemType;
+	}
+	
+	public function setBarcode($barcode){
+		$this->barcode = $barcode;
+	}
+	
+	public function setName($name){
+		$this->name = $name;
+	}
+	
+	public function setDescription($description){
+		$this->description = $description;
+	}
+	
+	public function setCondition($condition){
+		$this->condition = $condition;
+	}
+	
+	public function setCheckoutTo($checkoutTo){
+		$this->checkoutTo = $checkoutTo;
+	}
 
-      $list = array("a_barcode"=>$this->aBarcode, "a_name"=>$this->aName, "a_description"=>$this->aDescription, "a_condition"=>$this->aCondition, "a_checkout_out_to"=>$this->aCheckoutOutTo, "a_box"=>$this->aBox, "a_item_type"=>$this->aItemType);
-      $sql = "insert into assets values (";
-      foreach ($list as $key => $value){
-        if(is_string($value))
-          $sql .= "'$value', ";
-        else
-          $sql .= "$value, ";
-      }
-      $sql = substr($sql, 0, -2).")";
-      return mysql_query($sql, $this->conn);
-    
-}
-/**
- * Update this object into the DB
- * @return number of updated records
- */
-public function update(){
+	public function setBox($box){
+		$this->box = $box;
+	}
+	
+	public function setItemType($itemType){
+		$this->itemType = $itemType;
+	}
 
-      $list = array("a_barcode"=>$this->aBarcode, "a_name"=>$this->aName, "a_description"=>$this->aDescription, "a_condition"=>$this->aCondition, "a_checkout_out_to"=>$this->aCheckoutOutTo, "a_box"=>$this->aBox, "a_item_type"=>$this->aItemType);
-      $sql = "update assets set ";
-      foreach ($list as $key => $value){
-        if(is_string($value))
-          $sql .= "$key='$value', ";
-        else
-          $sql .= "$key=$value, ";
-      }
-      $sql = substr($sql, 0, -2)." where `a_barcode`='$aBarcode'";
-      return mysql_query($sql, $this->conn);
-    
-}
-/**
- * Search for items by item type
- * @param item type to look for
- */
-public function searchByType($itemType){
+	/**
+	 * Returns the string representation of this obbject
+	 * @return String repesentation ofAssets
+	 */
+	public function toString(){
+	  $s = '';
+	  $s .= 'a_barcode: '.$this->barcode;
+	  $s .= 'a_name: '.$this->name;
+	  $s .= 'a_description: '.$this->description;
+	  $s .= 'a_condition: '.$this->condition;
+	  $s .= 'a_checkout_to: '.$this->checkoutTo;
+	  $s .= 'a_box: '.$this->box;
+	  $s .= 'a_item_type: '.$this->itemType;
+	  return $s;
+	}
+	private function init($row){
+		$this->barcode = $row['a_barcode'];
+		$this->name = $row['a_name'];
+		$this->description = $row['a_description'];
+		$this->condition = $row['a_condition'];
+		$this->checkoutTo = $row['a_checkout_to'];
+		$this->box = $row['a_box'];
+		$this->itemType = $row['a_item_type'];
+	}
 
-      $rows = mysql_query("select * from assets where a_item_type = $itemType", $this->conn);
-      $assetss = array();
-      while ($row = mysql_fetch_array($rows)) {
-        $d = new Assets($this->conn);
-        $d->init($row);
-        array_push($assetss,$d);
-      }
-      return $assetss;      
-    
-}
+	public function loadFromPage(){
+	  if (isset($_POST['barcode'])){
+	    $this->barcode = $_POST['barcode'];
+	  }
+	  if (isset($_POST['name'])){
+	    $this->name = $_POST['name'];
+	  }
+	  if (isset($_POST['description'])){
+	    $this->description = $_POST['description'];
+	  }
+	  if (isset($_POST['condition'])){
+	    $this->condition = $_POST['condition'];
+	  }
+	  if (isset($_POST['checkoutTo'])){
+	    $this->checkoutTo = $_POST['checkoutTo'];
+	  }
+	  if (isset($_SESSION['box'])) {
+	  	$this->box = $_SESSION['Box'];
+	  } else if (isset($_POST['box'])) {
+	    $this->box = $_POST['box'];
+	  }
+	  if (isset($_SESSION['itemtype'])){
+	    $this->itemType = $_SESSION['itemtype'];
+	  }
+	  echo $this->toString();
+	}
+
+	/**
+	 * Create a form for inputing the fields
+	 */
+	public function printForm($action) {
+		$itemType = $_GET['itemtype'];
+		$query = "
+		SELECT c_index, c_value
+		FROM conditions;";
+		$this->connection->query($query);
+		echo "<center>
+		<form action=\"index.php?action=$action&type=asset\" method=\"post\" enctype=\"multipart/form-data\">
+		Barcode: <br>
+		<input type=\"text\" name=\"barcode\" value=\"$this->barcode\"> <br>
+		Name: <br>
+		<input type=\"text\" name=\"name\" value=\"$this->name\"> <br>
+		Description: <br>
+		<textarea name=\"description\" rows=\"10\" cols=\"60\">$this->description</textarea> <br>
+		Box: <br>
+		<input type=\"text\" name=\"box\"value=\"$this->box\"> <br>
+		Checked out to(Leave blank if no one): <br>
+		<input type=\"text\" name=\"checkoutTo\" value=\"$this->checkoutTo\"> <br>
+		Condition: <br>
+		<select name=\"condition\"";
+		//list conditions
+		while($row = $this->connection->fetch_row())
+		{
+			$index = $row[0];
+			$value = $row[1];
+			if($index == $this->condition)
+			{
+				echo "<option value=\"$index\" selected=\"yes\">$value</option>";
+			} else {
+				echo "<option value=\"$index\">$value</option>";
+			}
+		}
+		echo "</select> <br><br>
+		<input type=\"submit\" name=\"submit\" value=\"Finished\">
+		</center>";
+		
+	}	
+	/**
+	 * Insert this object into the DB
+	 * @return new id (auto increment value) genereated
+	 */
+
+	public function insert() {
+		$list = array("a_barcode"=>$this->barcode, "a_name"=>$this->name, "a_description"=>$this->description, "a_condition"=>$this->condition, "a_checkout_to"=>$this->checkoutTo, "a_box"=>$this->box, "a_item_type"=>$this->itemType);
+		$sql = "insert into assets values (";
+		foreach ($list as $key => $value){
+			if(is_string($value)) {
+				$sql .= "'$value', ";
+			} else {
+				$sql .= "$value, ";
+			}
+		}
+		$sql = substr($sql, 0, -2).")";
+		$this->connection->query($sql);
+	}
+	/**
+	 * Update this object into the DB
+	 * @return number of updated records
+	 */
+	public function update() {	
+		$list = array("a_barcode"=>$this->barcode, "a_name"=>$this->name, "a_description"=>$this->description, "a_condition"=>$this->condition, "a_checkout_to"=>$this->checkoutTo, "a_box"=>$this->box, "a_item_type"=>$this->itemType);
+		$sql = "update assets set ";
+		foreach ($list as $key => $value) {
+			if(is_string($value)) {
+				$sql .= "$key='$value', ";
+			} else {
+				$sql .= "$key=$value, ";
+			}
+		}
+		$sql = substr($sql, 0, -2)." where `a_barcode`='$barcode'";
+		$this->connection->query($sql);
+	}
+	
+	public function loadEntry($barcode) {
+		$query = "
+		SELECT a_barcode, a_name, a_description, a_condition, a_checkoutto, a_box, a_item_type
+		FROM assets
+		WHERE a_barcode = $barcode";
+		$this->connection->query($query);
+		$this->init($this->connection->fetch_row());	
+	}
+	
 }
 ?>
