60e957345413c160a6ae5a82ba64c17c91aa00d5
Dylan Enloe <denloe@cs.pdx.edu>
2009-10-17 23:48:56 -0700

    Added index.php, the hub of the website

---
+++ index.php
@@ -0,0 +1,65 @@
+<?php
+
+function __autoload($class_name) {
+    require_once $class_name . '.php';
+}
+
+$user = new User();
+
+$connection = new Connection();
+
+$webpage = new Webpage("ConAsset inventory management system");
+
+//$asset = new Asset($connection);
+
+if(isset($_GET['action']) && isset($_GET['type'])) {
+	if($_GET['type'] == 'asset') {
+		$asset = new Asset($connection);
+		if(isset($_POST['submit']))
+		{
+			if($_GET['action'] == 'add') {
+				$asset->loadFromPage();
+				$asset->insert();
+			} else if($_GET['action'] == 'edit') {
+				$asset->loadFromPage();
+				$asset->update();
+			}
+		} else {
+			if($_GET['action'] == 'edit') {
+				$asset->loadEntry($_GET['barcode']);
+			}
+			$asset->printForm($_GET['action']);	
+		}
+	}
+	if($_GET['type'] == 'assettype') {
+		$assettype = new AssetType($connection);
+		if(isset($_POST['submit']))
+		{
+			if($_GET['action'] == 'add') {
+				$assettype->loadFromPage();
+				$assettype->insert();
+			} else if($_GET['action'] == 'edit') {
+				$assettype->loadFromPage();
+				$assettype->update();
+			}
+		} else {
+			if($_GET['action'] == 'edit') {
+				$assettype->loadEntry($_GET['index']);
+			}
+			$assettype->printForm($_GET['action']);
+		}
+	}
+}
+
+echo "
+<center>
+<br><br>";
+$webpage->addURL("index.php?action=add&type=assettype",
+	"Create new asset type");
+echo "<br>";
+$webpage->addURL("index.php?action=add&type=asset",
+	"Add a new asset");
+echo "</center>";
+
+
+?>
